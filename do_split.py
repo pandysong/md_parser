38bb9c284be5b09281fbce309680810e8bf0b9b6
pandysong <pandy.song@gmail.com>
2018-08-21 22:01:42 +1000

    Add usage printing if there is not enough parameters

--- do_split.py
+++ do_split.py
@@ -1,8 +1,19 @@
+import sys
 import md_parser
 import md_split
 
-p = md_parser.parse('../CppCoreGuidelines/CppCoreGuidelines.md')
-for fn in md_split.split(p, split_level=1,
-                         out_path='../CppCoreGuidelines_SmallFiles/guidelines'):
-    print("writen file:", fn)
-    pass
+
+def do_split(markdown_file, output_path, verbose=0):
+    p = md_parser.parse(markdown_file)
+    for fn in md_split.split(p, split_level=1,
+                             out_path=output_path):
+        if verbose:
+            print("writen file:", fn)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('usage:\n'
+              '  python {} input.md output_path'.format(sys.argv[0]))
+    else:
+        do_split(sys.argv[1], sys.argv[2])
